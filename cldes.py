d93a77521a3047ca4f42480b283d24eb197c5818
Guilherme <guilherme.sergio@ufv.br>
2022-10-19 08:37:22 -0300

    test_entropy

--- cldes.py
+++ cldes.py
@@ -1,7 +1,6 @@
 import pandas as pd
-from pandas.api.types import is_object_dtype
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier
+from scipy.stats import entropy
 
 class CLDES:
 
@@ -9,9 +8,17 @@ class CLDES:
         self._min_per = min_per
         self._num_bins = num_bins
     
+    def _entropy1(self, labels, base=None, counts=None):
+        
+        value,counts = np.unique(labels, return_counts=True)
+        return entropy(counts)
+
     def generate_cluster_description(self, data: pd.DataFrame, cluster: int):
-        percentages = {}
-        continuos_vars = [x for x in data.columns if len(data[x].unique()) > 10]
+        data = data.dropna(axis=1)
+
+        numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+        cols = data.select_dtypes(numerics).columns
+        continuos_vars = [x for x in cols if len(data[x].unique()) > 10 and data[x].dtype and x != "cluster"]
         
         for col in continuos_vars: 
             bins = np.histogram_bin_edges(data[col], bins="fd")
@@ -25,8 +32,10 @@ class CLDES:
             percs_idx = percs[percs >= self._min_per].index
             percs = percs[percs_idx].values
             values = value_counts[percs_idx].values    
-
-            print("atributo: ", col)
+            
+            print("Entropy: ", entropy(value_counts.values, ))
+            print("")
+            print(col)
             print(pd.DataFrame({
                 "porc.": percs,
                 "valores": percs_idx
--- examples/cdes.py
+++ examples/cdes.py
@@ -7,14 +7,20 @@ sys.path.insert(0, path + "/cluster_explainability")
 from cldes import CLDES
 from sklearn.datasets import load_iris
 import pandas as pd
-from sklearn.tree import plot_tree
 
 
 iris = load_iris()
 iris_df = pd.DataFrame(data= np.c_[iris['data'], iris['target']],
                      columns= iris['feature_names'] + ['cluster'])
 
-cldes = CLDES(0.1, 10)
+cldes = CLDES(0, 10)
 
 cldes.generate_cluster_description(iris_df, 1)
+
+#df = pd.read_csv("/home/guilherme/Documentos/porto_seguro/data/personas_new.csv", index_col=0)
+#print(df.head())
+
+#print(df.info())
+
+#cldes.generate_cluster_description(df.drop("cod_pessoa", axis=1), 1)
 
