0bd2feb173f83e669234cd86a035b70a66c1c379
Guilherme <guilherme.sergio@ufv.br>
2022-11-07 10:32:30 -0300

    added svm feature importance

--- cldes.py
+++ cldes.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import entropy
+from sklearn import svm 
+from sklearn.metrics import precision_score
 
 class CLDES:
 
@@ -8,6 +10,38 @@ class CLDES:
         self._min_per = min_per
         self._num_bins = num_bins
         self.model = model
+
+
+    def explain_it(self, X, Y_true, X_test, y_test, n_repeats, groups, cluster):
+        clf = svm.SVC(decision_function_shape='ovo')
+        clf.fit(X, Y_true) 
+        
+
+        Pct_Chg = np.zeros((n_repeats,len(np.unique(groups)))) # preallocate output matrix number of repeats x number of features
+       
+        y_predicted = clf.predict(X_test)
+        e_original = precision_score(y_test, y_predicted, average=None)
+
+        print("------ Inicio da permutação --------")
+        for j in np.unique(groups): 
+            for k in range(n_repeats): 
+                np.random.seed(seed=k)
+                X_2 = np.copy(X_test)
+                X_2[:] = X_test[:] 
+                
+                Sub_Data = np.random.permutation(X_2[:, np.squeeze(list(groups == j*np.ones_like(groups)))]) # realiza a permutação
+                X_2[:, np.squeeze(list(groups == j*np.ones_like(groups)))] = Sub_Data # adiciona a permutaão na matriz
+                
+                Y_2 = clf.predict(X_2)
+                e_new = precision_score(y_test, Y_2, average=None)
+
+                e_new = e_new[cluster]
+                e_original_cluster = e_original[cluster]
+
+                # calcular a porcentagem de mudança na precisão
+                Pct_Chg[k,j] = e_original_cluster - e_new 
+        
+        return Pct_Chg
 
     def group_permutation_change(self, X, Y, n_repeats, groups, random_state, cluster, check_var):
         
@@ -31,10 +65,18 @@ class CLDES:
                 if check_var == 1:
                     Record[:,:,k] = Sub_Data # keep track of values for each permuted feature
 
+                if(j == 1 and k == 0):
+                    print(X_2)
+                    print(X)
+
                 Y_CLUSTER = Y == cluster
-                Y_2 = self.model.predict(X)
-                # print(np.sum(np.array(Y)!=np.array(Y_2))/len(np.squeeze(Y)))
-                Pct_Chg[k,j] = np.sum(np.array(Y[Y_CLUSTER])!=np.array(Y_2[Y_CLUSTER]))/len(np.squeeze(Y)) # calculate percent change
+                Y_2 = self.model.predict(X_2)
+
+                Y_ = Y[Y_CLUSTER] # contar a pct de mudança apenas para determinado grupo
+                Y_2_ = Y_2[Y_CLUSTER]
+                
+                #print(np.sum(np.array(Y)!=np.array(Y_2))/len(np.squeeze(Y)))
+                Pct_Chg[k,j] = np.sum(np.array(Y_)!=np.array(Y_2_))/len(np.squeeze(Y)) # calculate percent change
                 if check_var == 1:
                     R[:,j] += np.squeeze(np.array(Y)!=np.array(Y_2))/n_repeats
                     VarData[:,j] = np.mean(np.var(Record,axis= 2),axis=1)
@@ -49,7 +91,7 @@ class CLDES:
 
     def _entropy1(self, labels, base=None, counts=None):
         
-        value,counts = np.unique(labels, return_counts=True)
+        value, counts = np.unique(labels, return_counts=True)
         return entropy(counts)
 
     def generate_cluster_description(self, data: pd.DataFrame, cluster: int):
